033cdd4bfa996b9e44e6527c3d1b41cf9a95238e
Shrinidhikm <kmshrinidhi338@gmail.com>
2025-10-06 23:21:53 +0530

    Update User.js

---
+++ backend/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  role: {
+    type: String,
+    enum: ['student', 'alumni', 'faculty', 'admin'],
+    required: true
+  },
+  profile: {
+    firstName: String,
+    lastName: String,
+    bio: String,
+    profilePhoto: String,
+    skills: [String],
+    graduationYear: Number,
+    department: String,
+    linkedIn: String,
+    github: String,
+    currentPosition: String,
+    company: String
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  verificationToken: String,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+}, {
+  timestamps: true
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+module.exports = mongoose.model('User', userSchema);
